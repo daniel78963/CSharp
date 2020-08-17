f382049b095ceeb855e8c89cadb06d9e4c285c73
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2020-08-17 15:08:19 -0500

    staticClass Email Client

---
+++ CSharp/CSharp.ModelCore/StaticClass/EmailClient.cs
@@ -0,0 +1,21 @@
+﻿ 
+namespace CSharp.ModelCore.StaticClass
+{
+    using System;
+
+    static class EmailClient
+    {
+        static string sender { get; set; }
+        static EmailClient()
+        {
+            sender = "info@dotnettricks.com";
+            Console.WriteLine("Static constructor called");
+        }
+
+        public static void SendEmail(string receiver)
+        {
+            // TO DO: Write code for sending email
+            Console.WriteLine("Email sent to {0} from {1}", receiver, sender);
+        }
+    }
+}
---
+++ CSharp/CSharp.ModelCore/StaticClass/ProgramEmailClient.cs
@@ -0,0 +1,12 @@
+﻿ 
+namespace CSharp.ModelCore.StaticClass
+{
+    class ProgramEmailClient
+    {
+        static void Main()
+        {
+            //calling class members
+            EmailClient.SendEmail("mohan@gmail.com");
+        }
+    }
+}
