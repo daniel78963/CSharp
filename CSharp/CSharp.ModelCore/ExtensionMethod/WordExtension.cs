229732e83c81fc880e87f770146a5b3b7a939f5c
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2020-08-18 17:00:28 -0500

    Partial Class

--- CSharp/CSharp.ModelCore/ExtensionMethod/WordExtension.cs
+++ CSharp/CSharp.ModelCore/ExtensionMethod/WordExtension.cs
@@ -1,10 +1,22 @@
-﻿ 
+﻿
 namespace CSharp.ModelCore.ExtensionMethod
 {
-    using System;
-
     public static class WordExtension
     {
+        //Extension Method
+        //Used to add new behaviours to an existing class, struct or interface without modifying, deriving or recompiling the original type.
+        //A special type of static method in a static class.
+        //"this" keyword is used with the first parameter and the type of the first parameter will be the extended type.
+        //Called like as an instance method of the extended type.
+
+        //When to Use Extension Method
+        //An extension method is useful when you want to add new methods to a class, struct or interface within in a DLL or framework.
+
+        //Key Points About Extension Method
+        //An extension method is a special static method but it is called like an instance method.
+        //An extension method having the same name and signature like as an instance method will never be called since it has low priority than the instance method.
+        //An extension method couldn't override the existing instance method.
+
         public static int WordCount(this string str)
         {
             string[] strArray = str.Split(' ');
---
+++ CSharp/CSharp.ModelCore/PartialClass/Calculator.cs
@@ -0,0 +1,40 @@
+﻿
+namespace CSharp.ModelCore.ExtensionMethod
+{
+    using System;
+
+    //Partial Class
+    //A Partial class, interface and structure were introduced in C# 2.0. Now, it is possible to split the definition of a class, interface or structure over more than one source files. These source files must be defined in the same namespace or assembly and must have the partial keyword and same access modifiers.
+    //The above two partial classes would be combined into one at compile time as given 
+
+    //When to use Partial
+    //Allow more than one developer to work simultaneously on the same class, struct or interface.
+    //Partial class are particularly helpful for customizing auto-generated code by the IDE.Whenever the IDE generate the code then the tool may define some partial class, interface, methods and further customization of a partial class, the interface is done by the developers without messing with the system generated code.
+
+    //Key Points about Partial
+    //During code compilation, all the files should be available to form the final class, interface or struct.
+    //Any member declared in the one part/file will be available to all other parts.
+    //If any part has Inheritance, then it applies to the entire class.
+    //Different parts of a class or struct may inherit from different interfaces.
+    //If any part is declared abstract, then the whole class, interface or struct is considered abstract.
+    //If any part is declared sealed, then the whole class, interface or struct is considered sealed.
+
+    // Partial Class Part1
+    partial class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+
+
+    // Partial Class Part2
+    partial class Calculator
+    {
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
