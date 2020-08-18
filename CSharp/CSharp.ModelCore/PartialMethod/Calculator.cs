0c7c875327321d5838adfca1c892aa96cbd217e0
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2020-08-18 18:34:28 -0500

    partial method

---
+++ CSharp/CSharp.ModelCore/PartialMethod/Calculator.cs
@@ -0,0 +1,26 @@
+﻿ 
+namespace CSharp.ModelCore.PartialMethod
+{
+    using System;
+
+    // Partial Class Part1
+    partial class Calculator
+    {
+        partial void PartialAdd(int x, int y); //declaration
+    }
+
+    // Partial Class Part2
+    partial class Calculator
+    {
+        public void Add(int x, int y)
+        {
+            PartialAdd(x, y);
+        }
+
+        partial void PartialAdd(int x, int y) //implementation
+        {
+            int sum = x + y;
+            Console.WriteLine("Sum of {0} and {1} is : {2}", x, y, sum);
+        }
+    }
+}
---
+++ CSharp/CSharp.ModelCore/PartialMethod/ProgramPartialMethod.cs
@@ -0,0 +1,18 @@
+﻿
+namespace CSharp.ModelCore.PartialMethod
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class ProgramPartialMethod
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 4, num2 = 2;
+
+            Calculator calc = new Calculator();
+            calc.Add(num1, num2);
+        }
+    }
+}
